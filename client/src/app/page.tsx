8c06803d5326d4d832fca175f50f50fdf0f0f07b
xBl1nGy <s.hallouche@outlook.fr>
2024-09-21 21:42:10 +0200

    Initial setup
    Initial CSS and Tailwind config and dependencies installation

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        hi
+    </main>
+  );
+}
